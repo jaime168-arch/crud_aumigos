f1ef43174f7214064a9add8ea88e1e453fbdca8d
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-08-24 10:53:49 -0300

    uptade- conexão ok

--- infra/conexao.php
+++ infra/conexao.php
@@ -3,3 +3,11 @@ $host = 'localhost';
 $db = 'aumigos';
 $usuario = 'root';
 $senha = 'root';
+
+$conn = mysqli_connect($host, $usuario, $senha, $db);
+
+if (!$conn) {
+    die("Erro de conexão: " . mysqli_connect_erro());
+}
+
+mysqli_set_charset($conn, "utf8mb4");
