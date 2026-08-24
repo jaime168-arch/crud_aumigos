fc33bf0e67d913ff83f326b977423d479ee9305e
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-08-24 11:29:38 -0300

    add- iniciando o index

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// Index.php (Raiz do projeto)
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
